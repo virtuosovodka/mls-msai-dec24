0ac3f909016165c52989fadb086415f0cb79357d
virtuosovodka <67210448+virtuosovodka@users.noreply.github.com>
2025-03-27 09:43:07 -0700

    Update prep.py

--- CICD-project- FullCode/data-science/src/prep.py
+++ CICD-project- FullCode/data-science/src/prep.py
@@ -15,11 +15,11 @@ import mlflow
 def parse_args():
     '''Parse input arguments'''
 
-    parser = argparse.__________("prep")  # Create an ArgumentParser object
-    parser.add_argument("--raw_data", type=_____, help="Path to raw data")  # Specify the type for raw data (str)
-    parser.add_argument("--train_data", type=_____, help="Path to train dataset")  # Specify the type for train data (str)
-    parser.add_argument("--test_data", type=_____, help="Path to test dataset")  # Specify the type for test data (str)
-    parser.add_argument("--test_train_ratio", type=______, default=_____, help="Test-train ratio")  # Specify the type (float) and default value (0.2) for test-train ratio
+    parser = argparse.ArgumentParser("prep")  # Create an ArgumentParser object
+    parser.add_argument("--raw_data", type=str, help="Path to raw data")  # Specify the type for raw data (str)
+    parser.add_argument("--train_data", type=str, help="Path to train dataset")  # Specify the type for train data (str)
+    parser.add_argument("--test_data", type=str, help="Path to test dataset")  # Specify the type for test data (str)
+    parser.add_argument("--test_train_ratio", type=float, default=0.2, help="Test-train ratio")  # Specify the type (float) and default value (0.2) for test-train ratio
     args = parser.parse_args()
 
     return args
@@ -36,19 +36,33 @@ def main(args):  # Write the function name for the main data preparation logic
     # Step 2: Split the dataset into training and testing sets using train_test_split with specified test size and random state.  
     # Step 3: Save the training and testing datasets as CSV files in separate directories for easier access and organization.  
     # Step 4: Log the number of rows in the training and testing datasets as metrics for tracking and evaluation.  
+    label_encoder = LabelEncoder()
+    df['Type'] = label_encoder.fit_transform(df['Type'])
 
+    # Split Data into train and test datasets
+    train_df, test_df = train_test_split(df, test_size=args.test_train_ratio, random_state=42)
+
+    # Save train and test data
+    os.makedirs(args.train_data, exist_ok=True)
+    os.makedirs(args.test_data, exist_ok=True)
+    train_df.to_csv(os.path.join(args.train_data, "train.csv"), index=False)
+    test_df.to_csv(os.path.join(args.test_data, "test.csv"), index=False)
+
+    # Log the metrics
+    mlflow.log_metric('train size', train_df.shape[0])
+    mlflow.log_metric('test size', test_df.shape[0])
 
 if __name__ == "__main__":
     mlflow.start_run()
 
     # Parse Arguments
-    args = _______()  # Call the function to parse arguments
+    args = parse_args()  # Call the function to parse arguments
 
     lines = [
-        f"Raw data path: {args._______}",  # Print the raw_data path
-        f"Train dataset output path: {args._______}",  # Print the train_data path
-        f"Test dataset path: {args._______}",  # Print the test_data path
-        f"Test-train ratio: {args._______}",  # Print the test_train_ratio
+        f"Raw data path: {args.raw_data}",  # Print the raw_data path
+        f"Train dataset output path: {args.train_data}",  # Print the train_data path
+        f"Test dataset path: {args.test_data}",  # Print the test_data path
+        f"Test-train ratio: {args.test_train_ratio}",  # Print the test_train_ratio
     ]
 
     for line in lines:
